1c06779847296c8e930457f23bb52032ab842b4b
liliia87 <lilia.krivelyova@gmail.com>
2022-03-29 19:38:34 -0700

    add 4 new functions:addition, substraction, dividing and multiplication

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,20 @@ function add(number1, number2){
     return number1+number2;
     
     }
-    const number1 = parseInt(prompt("Enter a number: "));
-    const number2 = parseInt(prompt("Enter another number: "));
-    alert(add(number1, number2));
+     
+function subtract(number1, number2){
+    return number1-number2;
+}     
+    
+function multiply(number1, number2) {
+    return number1*number2;
+}
+
+function divide(number1, number2) {
+    return number1/number2;
+}
+
+const number1 = parseFloat(prompt("Enter a number: "));
+const number2 = parseFloat(prompt("Enter another number: "));
+alert(divide(number1, number2));
+
